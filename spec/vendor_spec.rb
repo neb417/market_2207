15ddc83a332dd7ece6659a625e4febf2aa42ba86
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-08-08 09:24:36 -0600

    Complete iteration 1

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -11,30 +11,30 @@ RSpec.describe Vendor do
       expect(vendor).to be_a(Vendor)
     end
 
-    xit 'Vendor has attributes' do
+    it 'Vendor has attributes' do
       expect(vendor.name).to eq('Rocky Mountain Fresh')
       expect(vendor.inventory).to eq({})
     end
 
-    xit 'Vendor stock is empty by default' do
+    it 'Vendor stock is empty by default' do
       expect(vendor.check_stock(item1)).to eq(0)
     end
 
-    xit 'Vendor can stock Items' do
+    it 'Vendor can stock Items' do
       vendor.stock(item1, 30)
 
-      expect(vendor.inventory).to eq({item1: 30})
+      expect(vendor.inventory).to eq({item1 => 30})
       expect(vendor.check_stock(item1)).to eq(30)
     end
 
-    xit 'Vendor Invetory adds to items' do
+    it 'Vendor Invetory adds to items' do
       vendor.stock(item1, 30)
       vendor.stock(item1, 25)
       expect(vendor.check_stock(item1)).to eq(55)
 
       vendor.stock(item2, 12)
       
-      expect(vendor.inventory).to eq({item1: 55, item2: 12})
+      expect(vendor.inventory).to eq({item1 => 55, item2 => 12})
     end
   end
 end
